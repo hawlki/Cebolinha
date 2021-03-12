1a859dd9f2308e1fd42a52ea90e84aebbba7629d
israel lopes <israel.lopes2710@gmail.com>
2021-03-12 10:27:14 -0300

    finaliza

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Beep();
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Etec Adolpho Berezin");
+            Console.WriteLine("Feito por Israel Camilo Lopes");
+            Console.WriteLine("1I3");
+            Console.WriteLine("-----------------------------");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("Por favor, digite alguma coisa que tenha o uso da letra \"R\":");
+            String algumaCoisa = Console.ReadLine();
+            algumaCoisa = algumaCoisa.Replace("r","l");
+            algumaCoisa = algumaCoisa.Replace("R","L");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("clique em uma tecla para continuar");
+            Console.ReadKey();
+            Console.WriteLine("----------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("Veja como uma pessoa com dislaila falaria:");
+            Console.WriteLine($"{algumaCoisa}");
+            Console.ResetColor();
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("clique em uma tela para encerrar este programa");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
